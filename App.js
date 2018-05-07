243b171dcc3020ecc9854633561ee4979f06f3c6
Faisal Khan <faisal.khan@kelltontech.com>
2018-05-07 19:29:57 +0530

    extra code removed

--- App.js
+++ App.js
@@ -12,29 +12,6 @@ import {View} from 'react-native';
 
 export default class App extends Component {
 
-  componentDidMount() {
-    AppState.addEventListener('change', this._handleAppStateChange);
-  }
-
-  // componentWillUnmount() {
-  //   AppState.removeEventListener('change', this._handleAppStateChange);
-  // }
-
-  _handleAppStateChange = (nextAppState) => {
-    // if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
-      if (state === 'active') {
-        console.log("---->","state active");
-       }else if(state === 'background'){
-        console.log("---->","background");
-      }else{
-        console.log("---->","nothing");
-      }
-    // }else{
-    //   console.warn("----->","wth");
-    // }
-    // this.setState({appState: nextAppState});
-  }
-
   render() {
     return (
         <View>
